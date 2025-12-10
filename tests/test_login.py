0e8e05a525f959d9805844307e24304d01f41de4
Dmytro Litvinov <dmlitvinov99@gmail.com>
2025-12-10 16:43:25 -0500

    added test login with invalid password

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,3 +13,13 @@ def test_successful_login(page: Page, base_url: str):
     assert inventory_page.is_loaded(), "Inventory page should be visible after login"
 
     assert inventory_page.get_items_count() > 0, "There should be at least one item in inventory"
+
+
+def test_login_with_invalid_password(page: Page, base_url: str):
+    login_page = LoginPage(page, base_url)
+
+    login_page.open()
+    login_page.login("standard_user", "wrong_password")
+
+    error_text = login_page.get_error_message()
+    assert "WRONG_MESSAGE" in error_text or "Username and password do not match" in error_text
